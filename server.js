d9e6da50a400b20c182c38997291a0150cad0e73
Jossef Harush <jossef12@gmail.com>
2015-01-10 20:42:56 +0200

    adding files

--- server.js
+++ server.js
@@ -1,102 +1,92 @@
-'use strict';
-/**
- * Module dependencies.
+(function () {
+    'use strict';
+    var expressPort = 8080;
+    var expressIPAddress = '0.0.0.0';
+    var mqttPort = 1883;
+    var mqttIPAddress = '0.0.0.0';
+    var mongoDBUrl = 'mongodb://127.0.0.1:27017/IsOccupied';
 
-var init = require('./config/init')(),
-	config = require('./config/config'),
-	mongoose = require('mongoose'),
-	chalk = require('chalk');
-/**
- * Main application entry file.
- * Please note that the order of loading is important.
+    var mqtt = require('mqtt');
+    var chalk = require('chalk');
+    var express = require("express");
+    var path = require("path");
+    var expressApp = express();
+    var http = require('http').Server(expressApp);
+    var io = require('socket.io')(http);
 
 
-// Bootstrap db connection
-var db = mongoose.connect(config.db, function(err) {
-	if (err) {
-		console.error(chalk.red('Could not connect to MongoDB!'));
-		console.log(chalk.red(err));
-	}
-});
-// Init the express application
-var app = require('./config/express')(db);
+    var occupiedCache = {};
 
-// Bootstrap passport config
-require('./config/passport')();
+    // --------------------------------
+    // MQTT
 
-// Start the app by listening on <port>
-app.listen(config.port);
+    var mqttServer = mqtt.createServer(function (client) {
+        client.on('connect', function (packet) {
+            client.id = packet.clientId;
+            client.connack({returnCode: 0});
+        });
 
-// Expose app
-exports = module.exports = app;
+        client.on('publish', function (packet) {
+            var data = JSON.parse(packet.payload);
+            occupiedCache[client.id] = data.occupied;
+            console.log(chalk.bold.cyan('INCOMING'), client.id, '->' , chalk.cyan(packet.payload));
 
-// Logging initialization
-console.log('MEAN.JS application started on port ' + config.port);
+            io.emit('message', packet.payload );
+        });
+    });
 
-*/
+    // --------------------------------
+    // Mongo DB
 
-var mqtt = require('mqtt');
+    var mongoClient = require('mongodb').MongoClient;
 
-mqtt.createServer(function(client) {
-	var self = this;
+    var db = mongoClient.connect(mongoDBUrl, function (err, db) {
+        if (err) {
+            console.error(chalk.red('Could not connect to MongoDB!'));
+            console.log(chalk.red(err));
+        }
+        else {
 
-	if (!self.clients) self.clients = {};
+            console.log(chalk.bold.green('CONNECTED'), 'mongodb', chalk.cyan(mongoDBUrl));
 
-	client.on('connect', function(packet) {
+            db.createCollection('history', function (err, collection) {
+            });
 
-		console.log('new client!', packet);
+            var collection = db.collection('history');
+            var doc1 = {'hello': 'doc1'};
+            var doc2 = {'hello': 'doc2'};
+            var lotsOfDocs = [{'hello': 'doc3'}, {'hello': 'doc4'}];
 
-		client.connack({returnCode: 0});
-		client.id = packet.clientId;
-		self.clients[client.id] = client;
-	});
+            collection.insert(doc2, {w: 1}, function (err, result) {
+            });
+            collection.insert(lotsOfDocs, {w: 1}, function (err, result) {
+            });
+        }
+    });
 
-	client.on('publish', function(packet) {
-		for (var k in self.clients) {
-			self.clients[k].publish({topic: packet.topic, payload: packet.payload});
-		}
-	});
+    // --------------------------------
+    // Express JS
 
-	client.on('subscribe', function(packet) {
-		var granted = [];
-		for (var i = 0; i < packet.subscriptions.length; i++) {
-			granted.push(packet.subscriptions[i].qos);
-		}
+    // Static files
+    expressApp.use('/static/', express.static(path.join(__dirname, 'static')));
 
-		client.suback({granted: granted, messageId: packet.messageId});
-	});
+    // REST API
+    expressApp.get("/api/messages/:id", function (request, response) {
 
-	client.on('pingreq', function(packet) {
-		client.pingresp();
-	});
+    });
 
-	client.on('disconnect', function(packet) {
-		client.stream.end();
-	});
+    // Root Page
+    expressApp.get("/", function (request, response) {
+        response.sendFile(path.join(__dirname, 'static', 'index.html'));
+    });
 
-	client.on('close', function(err) {
-		delete self.clients[client.id];
-	});
 
-	client.on('error', function(err) {
-		console.log('error!', err);
+    // --------------------------------
+    // Listen
+    http.listen(expressPort, expressIPAddress);
+    mqttServer.listen(mqttPort, mqttIPAddress);
 
-		if (!self.clients[client.id]) return;
+    console.log(chalk.bold.yellow('LISTENING'), 'express', chalk.cyan(expressIPAddress), chalk.cyan(expressPort));
+    console.log(chalk.bold.yellow('LISTENING'), 'mqtt', chalk.cyan(mqttIPAddress), chalk.cyan(mqttPort));
 
-		delete self.clients[client.id];
-		client.stream.end();
-	});
-}).listen(1883, '0.0.0.0');
-
-/*
-console.log('hi!');
-
-var client = mqtt.createClient();
-console.log('hi!');
-
-client.subscribe('messages');
-client.publish('messages', 'hello me!');
-client.on('message', function(topic, message) {
-	console.log(message);
-});
-client.options.reconne*/ctPeriod = 0;
+})();
